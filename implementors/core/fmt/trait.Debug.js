b071a3ec150b9872f4af406944d88eb6af9ba59b
pocketberserker <pocketberserker@gmail.com>
2017-01-10 01:15:19 +0900

    generate docs

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["zero_formatter"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='zero_formatter/enum.ZeroFormatterError.html' title='zero_formatter::ZeroFormatterError'>ZeroFormatterError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ zero_formatter/util/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["check_non_null",""]]});
